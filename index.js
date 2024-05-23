a477a4dc8d6dfc446e3585ad449c59419794c212
Alejandra Bolaños <118947573+ALEJABM0817@users.noreply.github.com>
2024-05-23 17:13:02 -0500

    prueba insercion a bd

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { PORT } from './config.js';
 import indexRoutes from './routes/index.routes.js';
 import usersRoutes from './routes/users.routes.js';
 const app = express();
+app.use(express.json())
 app.use(indexRoutes);
 app.use(usersRoutes);
 app.listen(PORT);
