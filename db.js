9426373024d0731becd0c3ec86319abb1d47afb8
Alejandra Bolaños <ana.alejandra.bolanos@correounivalle.edu.co>
2024-11-14 22:37:30 -0500

    pruebas para deploy

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 import { createPool } from 'mysql2/promise';
 
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
+console.log(process.env.DB_USER);
+
 export const pool = createPool({
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
