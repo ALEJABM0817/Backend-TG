0e8090eb8e86ae6aa9c3a62e992f538550857bcc
Alejandra Bolaños <ana.alejandra.bolanos@correounivalle.edu.co>
2024-11-18 21:19:54 -0500

    rutas

--- index.js
+++ index.js
@@ -8,14 +8,23 @@ import { PORT } from './config.js';
 import indexRoutes from './routes/index.routes.js';
 import usersRoutes from './routes/users.routes.js';
 import imagesRoutes from './helpers/saveImages.js';
+
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(indexRoutes);
 app.use(usersRoutes);
 app.use(imagesRoutes);
-app.use(express.static(path.join(__dirname, 'assets')));
-app.listen(PORT);
-console.log('Server', PORT, process.env.DB_PORT)
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port', PORT);
+});
